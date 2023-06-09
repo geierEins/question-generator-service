ade093ac0f482a3548aab85baed9e8ad29907ecb
geierEins <58027978+geierEins@users.noreply.github.com>
2023-06-09 23:32:51 +0200

    enable cross origin for *

--- src/main/java/de/geier/QuestionGeneratorService/controller/QuestionController.java
+++ src/main/java/de/geier/QuestionGeneratorService/controller/QuestionController.java
@@ -2,6 +2,7 @@ package de.geier.QuestionGeneratorService.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import de.geier.QuestionGeneratorService.loader.QuestionSupplier;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("/api")
 public class QuestionController {
 
@@ -24,7 +26,7 @@ public class QuestionController {
     	return "QuestionGeneratorService am Start ...";
     }
     
-
+    
 	@GetMapping(path = "/getRandomQuestion", produces = MediaType.APPLICATION_JSON_VALUE)
 	public String getRandomQuestion() {
 		return supplier.getCurrentQuestion().toString(); 
